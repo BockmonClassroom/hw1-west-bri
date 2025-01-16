5a7a6df49724ed82eade5118c05752f9708b6125
BrianWest <west.bri@northeastern.edu>
2025-01-15 22:45:25 -0500

    Added comments

--- hw1.py
+++ hw1.py
@@ -1,7 +1,8 @@
+# Brian West 1/15/2025
 # I haven't had the opportunity to learn python yet, so this code comes from a tutorial: https://www.geeksforgeeks.org/reading-csv-files-in-python/
 # Hopefully I can catch up
 import csv
-with open('data/Iris.csv', mode = 'r') as file:
-    csvFile = csv.reader(file)
-    for lines in csvFile:
+with open('data/Iris.csv', mode = 'r') as file: # open file as read only
+    csvFile = csv.reader(file) # create reader object from file
+    for lines in csvFile: # iterate through each line and print it
         print(lines)
